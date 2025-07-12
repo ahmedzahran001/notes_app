5f3c719e260cc21b6d15e8d9876e6d276220d506
ahmedzahran001 <ahmedzhran82@gmail.com>
2025-07-12 08:36:05 +0300

    create app bar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:NoteApp/views/widgets/custom_search_icon.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 30),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
