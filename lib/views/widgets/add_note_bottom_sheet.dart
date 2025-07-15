a21a2b3a2e7bc89ecf7e171b2310f0bad93b058f
ahmedzahran001 <ahmedzhran82@gmail.com>
2025-07-15 22:34:31 +0300

    finish notes bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:NoteApp/views/widgets/custom_button.dart';
 import 'package:NoteApp/views/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -8,40 +9,38 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 16),
-          Container(
-            width: 108,
-            height: 8,
-            clipBehavior: Clip.antiAlias,
-            decoration: ShapeDecoration(
-              color: const Color(0xFFFFEFD7),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Container(
+              width: 108,
+              height: 8,
+              clipBehavior: Clip.antiAlias,
+              decoration: ShapeDecoration(
+                color: const Color(0xFFFFEFD7),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 32),
-          CustomTextField(
-            hint: 'Title...',
-          ),
-          SizedBox(height: 24),
-          CustomTextField(
-            hint: 'Content...',
-            maxLines: 5,
-          ),
-        ],
+            SizedBox(height: 32),
+            CustomTextField(
+              hint: 'Title...',
+            ),
+            SizedBox(height: 24),
+            CustomTextField(
+              hint: 'Content...',
+              maxLines: 7,
+            ),
+            SizedBox(height: 32),
+            // Spacer(),
+            CustomButton(),
+            // Spacer(),
+            SizedBox(height: 32)
+          ],
+        ),
       ),
     );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
