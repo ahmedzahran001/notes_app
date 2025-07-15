b2ba6338a4f4e116731c6fdbd476f42049430841
ahmedzahran001 <ahmedzhran82@gmail.com>
2025-07-15 23:15:31 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,3 +1,4 @@
+import 'package:NoteApp/views/edit_note_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -6,60 +7,72 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 16, top: 24, left: 16, right: 8),
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFEFD7),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Flutter Tips',
-            style: TextStyle(
-              color: Color(0xFF2B2525),
-              fontSize: 18,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w800,
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 8, right: 9),
-            child: Text(
-              maxLines: 3,
-              'Lorem ipsum dolor sit am consectetur Lorem ipsum dolor sit am consectetur  ',
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(bottom: 16, top: 24, left: 16, right: 8),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFEFD7),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Flutter Tips',
               style: TextStyle(
-                color: Color(0xFF2A2525),
-                fontSize: 16,
+                color: Color(0xFF2B2525),
+                fontSize: 18,
                 fontFamily: 'Inter',
-                fontWeight: FontWeight.w400,
+                fontWeight: FontWeight.w800,
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'May21 ,2025',
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8, right: 9),
+              child: Text(
+                maxLines: 3,
+                'Lorem ipsum dolor sit am consectetur Lorem ipsum dolor sit am consectetur  ',
                 style: TextStyle(
                   color: Color(0xFF2A2525),
-                  fontSize: 14,
+                  fontSize: 16,
                   fontFamily: 'Inter',
-                  fontWeight: FontWeight.w300,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'May21 ,2025',
+                  style: TextStyle(
+                    color: Color(0xFF2A2525),
+                    fontSize: 14,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w300,
+                  ),
                 ),
-              ),
-            ],
-          )
-        ],
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
