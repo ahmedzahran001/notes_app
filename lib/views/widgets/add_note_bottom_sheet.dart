af2c480a9e52a2400d35125ec276ef4e9a250646
ahmedzahran001 <ahmedzhran82@gmail.com>
2025-07-20 20:45:09 +0300

    take input and validate data

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -7,39 +7,77 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 16),
-            Container(
-              width: 108,
-              height: 8,
-              clipBehavior: Clip.antiAlias,
-              decoration: ShapeDecoration(
-                color: const Color(0xFFFFEFD7),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          Container(
+            width: 108,
+            height: 8,
+            clipBehavior: Clip.antiAlias,
+            decoration: ShapeDecoration(
+              color: const Color(0xFFFFEFD7),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
               ),
             ),
-            SizedBox(height: 32),
-            CustomTextField(
-              hint: 'Title...',
-            ),
-            SizedBox(height: 24),
-            CustomTextField(
-              hint: 'Content...',
-              maxLines: 7,
-            ),
-            SizedBox(height: 32),
-            // Spacer(),
-            CustomButton(),
-            // Spacer(),
-            SizedBox(height: 32)
-          ],
-        ),
+          ),
+          const SizedBox(height: 32),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: 'Title...',
+          ),
+          const SizedBox(height: 24),
+          CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
+            hint: 'Content...',
+            maxLines: 7,
+          ),
+          const SizedBox(height: 32),
+          // Spacer(),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          // Spacer(),
+          const SizedBox(height: 32)
+        ],
       ),
     );
   }
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,26 +1,30 @@
-
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, this.onTap});
+
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 35,
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFEFD7),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Color(0xFF141414),
-            fontSize: 20,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 35,
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFEFD7),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Color(0xFF141414),
+              fontSize: 20,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
